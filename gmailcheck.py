3002bd6c96e7891656a8be8ce88a30694b1dc329
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2020-04-24 13:59:17 +0900

    Update gmailcheck.py

--- gmailcheck.py
+++ gmailcheck.py
@@ -25,7 +25,7 @@ def main():
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('gcredentials.json', SCOPES)
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('gmail', 'v1', http=creds.authorize(Http()))
     
