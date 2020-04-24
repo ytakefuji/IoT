c10aea9d7fe733405f5e28b1c880158b5cc2ff17
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2020-04-24 13:56:04 +0900

    Add files via upload

---
+++ gmailcheck.py
@@ -0,0 +1,41 @@
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+
+def ListMessagesMatchingQuery(service, user_id, query=''):
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               q=query).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id, q=query,
+                                         pageToken=page_token).execute()
+      messages.extend(response['messages'])
+    return messages
+  except:
+    print('An error occurred:')
+
+def main():
+   
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('gcredentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=creds.authorize(Http()))
+    
+    results = service.users().messages().list(userId='me',labelIds = ['INBOX']).execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        print("No messages found.")
+    else:
+        email_count = len(ListMessagesMatchingQuery(service, "me", "label:unread category:primary"))
+        print(email_count)
+if __name__ == '__main__':
+    main()
