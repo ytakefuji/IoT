4848e4f2b072b087b055a3423d81578d53b0dbef
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2020-06-02 15:48:02 +0900

    Update blink.py

--- blink.py
+++ blink.py
@@ -1,9 +1,10 @@
 import serial,os
-s=serial.Serial('com3',9600)
+from time import sleep
+s=serial.Serial("COM3",9600)
+print(s.readline().decode())
 while 1:
  i=input('enter: ')
  if i=='q':
   s.close()
   os._exit(0)
- s.write(int(i))
-
+ s.write(str.encode(i))
